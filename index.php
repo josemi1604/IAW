9ca3f98b793c10f03dc05c7cf0e4fffaa3947819
josemi1604 <146703855+josemi1604@users.noreply.github.com>
2023-10-09 09:46:41 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "!HOLA!";
+        // comentario
+        # comentario
+        /*comentario*/
+        echo "<h2>Titulo h2</h2>";
+        print "hola hecho con print";
+        echo " Comillas simples ' HOLA ' ";
+         echo ' Comillas dobles " HOLA "';
+             /*comentario*/
+        echo "<h1>Titulo h1</h1>";
+            /*comentario*/
+            $a = 4;
+            $b = 3;
+            echo $a + $b;
+                /*comentario*/
+          echo "<h1>Titulo h1</h1>";
+              /*comentario*/
+          
+          define("SALUDO"," Buenos Dias ");
+          echo SALUDO;
+          
+           echo "<h1>Titulo h1</h1>";
+           
+          const SALUDO2 = "Buenos Dias ";
+          echo SALUDO2;
+          
+          echo "<h1>Titulo h1</h1>";
+          
+                  $x = 5985;
+                  var_dump($x);
+                  echo "<h1>T</h1>";
+                  
+                  $y = 8.75;
+                  var_dump($y);
+                  echo "<h1>T</h1>";
+                  
+                  $z = false;
+                  var_dump($z);
+                   echo "<h1>T</h1>";
+                   
+                  $w = "Hi!";
+                  var_dump($w);
+                  
+
+        ?>
+    </body>
+</html>
